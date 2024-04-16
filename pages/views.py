dfbfbae198dfbd9125cfc4b8f4e27f562b6c47ca
Emmanuel Castañeda Cano <melcastacano@gmail.com>
2024-04-16 09:17:19 -0500

    with about page}

---
+++ pages/views.py
@@ -0,0 +1,38 @@
+from typing import Any
+from django.shortcuts import render
+from django.views.generic import TemplateView # new
+from django.http import HttpResponse # new
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Your Name",
+        })
+
+        return context
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact us - Online Store",
+            "subtitle": "Contact us",
+            "description": "This is a contact page ...",
+            "author": "Developed by: Your Name",
+        })
+
+        return context
