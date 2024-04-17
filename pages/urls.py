18288bcba57d8d3c45a75dfa09f01ea1385f313c
Emmanuel Castañeda Cano <ecastanedc@eafit.edu.do>
2024-04-16 22:19:12 -0500

    how Show Products

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView  # Modificado
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),  # Modificado
     path('about/', AboutPageView.as_view(), name='about'),  # Modificado
     path('contact/', ContactPageView.as_view(), name='contact'),  # Modificado
+    path('products/', ProductIndexView.as_view(), name='products'),  # Modificado
+    path('products/<int:id>/', ProductShowView.as_view(), name='product_show'),  # Modificado
 ]
